d8356d77a0569ef60c506b7ac629541ac3299554
Gabriel <gabriel.d.machado92@gmail.com>
2023-03-09 01:49:28 -0300

    Final code

--- lotrsdk/lotrsdk.py
+++ lotrsdk/lotrsdk.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 
 # Getting variables from env
-dotenv_path = join(dirname(__file__), '../prod.env')
+dotenv_path = join(dirname(__file__), 'prod.env')
 load_dotenv(dotenv_path)
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 BASE_URL_MOVIE = os.environ.get('BASE_URL_MOVIE')
